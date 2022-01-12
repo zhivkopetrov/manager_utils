90d137ea7bd47bec14da557f7f8226e4355ea9ed
zhivko.petrov <zhivko.petrov@ocado.com>
2022-01-12 11:52:50 +0200

    feat: add suport for install_and_export_target and enable_ros_tooling_for_target

---
+++ manager_utilsConfig.cmake
@@ -0,0 +1,14 @@
+include(CMakeFindDependencyMacro)
+
+#find_dependency will correctly forward REQUIRED or QUIET args to the consumer
+#find_package is only for internal use
+find_dependency(sdl_utils REQUIRED)
+
+if(NOT TARGET manager_utils::manager_utils)
+  include(${CMAKE_CURRENT_LIST_DIR}/manager_utilsTargets.cmake)
+endif()
+
+# This is for catkin compatibility.
+set(manager_utils_LIBRARIES manager_utils::manager_utils)
+get_target_property(manager_utils_INCLUDE_DIRS manager_utils::manager_utils INTERFACE_INCLUDE_DIRECTORIES)
+
